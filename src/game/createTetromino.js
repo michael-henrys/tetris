8e72a469f3419f8d48560631fe8edafdc260045b
michael henrys <michael42h@gmail.com>
2022-10-14 10:23:52 +1300

    add I tetronimo

--- src/game/createTetromino.js
+++ src/game/createTetromino.js
@@ -8,6 +8,21 @@ const T = {
     [1, 1]]
 }
 
+const I = {
+  type: 'I',
+  active: true,
+  cells:
+    [[0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0]]
+}
+
 export default function createTetromino() {
-  return T
+  //create a random number between 0 and 6
+  const random = Math.floor(Math.random() * 7)
+  //create a tetronimo based on the random number
+
+
+  return I
 }
