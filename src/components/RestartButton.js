aea1f5f1d9a340465756a84f74153bfee83a8046
michael henrys <michael42h@gmail.com>
2023-04-15 10:12:15 +1200

    Change reset button to caps

--- src/components/RestartButton.js
+++ src/components/RestartButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function RestartButton( {onClick } ) {
   return (
     <div onClick={onClick} className='restart-button button'>
-      Reset
+      RESET
     </div>
   )
 }
